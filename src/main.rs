3b13b46feed8ee03cb97cdaf0cc2f20ed5363802
Yuki <y-nakata@sakura.ad.jp>
2024-05-26 17:44:36 +0900

    Run wasmi with Sample-code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use wasmi::*;
+
+fn main() -> Result <()>{
+    let engine = Engine::default();
+    let wat = r#"
+    (module
+        (import "host" "hello" (func $host_hello (param i32)))
+        (func (export "hello")
+            (call $host_hello (i32.const 3))
+        )
+    )
+    "#;
+
+    let wasm = wat::parse_str(&wat)?;
+    let module = Module::new(&engine, &mut &wasm[..])?;
+
+    type HostState = u32;
+    let mut store = Store::new(&engine ,42);
+    let host_hello = Func::wrap(&mut store, |caller: Caller<'_, HostState>, param: i32| {
+        println!("Got {param} from WebAssembly");
+        println!("My host state is: {}", caller.data());
+    });
+
+    let mut linker = <Linker<HostState>>::new(&engine);
+    linker.define("host", "hello", host_hello)?;
+    let instance = linker
+        .instantiate(&mut store, &module)?
+        .start(&mut store)?;
+    let hello = instance.get_typed_func::<(), ()>(&store, "hello")?;
+    hello.call(&mut store, ())?;
+    Ok(())
 }
