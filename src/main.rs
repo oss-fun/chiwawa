d0bb00accaf37d8e1b94c02808223d0d8a28e50a
Yuki <y-nakata@sakura.ad.jp>
2024-06-03 19:20:08 +0900

    Refactor: Create Wasmer Instance

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,25 @@ use std::io::{Read, BufReader};
 use std::env;
 use crate::underRuntime::UnderRuntime;
 use anyhow::{Result};
-use wasmer::{Store, Module, Instance, Value, imports};
-use wasmer_wasix::WasiEnv;
-use std::thread::Builder;
+
 
 fn main() -> Result <()>{
     let args: Vec<String> = env::args().collect();
-    let path = &args[1];
+    let target = &args[1].as_str();
+    let path = &args[2];
 
-    let mut bytecode = Vec::new();
-    
+    let mut bytecode = Vec::new();    
     let file = File::open(path)?;
     let mut reader = BufReader::new(file);
 
-    //let mut buf =[0; 8];
-    //let _= reader.read(&mut buf);
-    //bytecode.extend(buf);
+    let mut buf =[0; 8];
+    let _= reader.read(&mut buf);
+    bytecode.extend(buf);
     //let lf: [u8; 1] = [0x0a];
     //bytecode.extend(lf.to_vec());
-    let _ = reader.read_to_end(&mut bytecode)?;
+    //let _ = reader.read_to_end(&mut bytecode)?;
 
-    //let mut runtime = underRuntime::Wasmi::new(bytecode);
-    let mut store = wasmer::Store::default();
-    let module = wasmer::Module::new(&store, &bytecode)?;
-
-    let runtime = tokio::runtime::Builder::new_multi_thread()
-    .enable_all()
-    .build()
-    .unwrap();
-    let _guard = runtime.enter();
-
-    let mut wasi_env = WasiEnv::builder("hello")
-        // .args(&["world"])
-        // .env("KEY", "Value")
-        .finalize(&mut store)?;
-    let import_object = wasi_env.import_object(&mut store, &module)?;
-    let instance = Instance::new(&mut store, &module, &import_object)?;
-    wasi_env.initialize(&mut store, instance.clone())?;
-    let func = instance.exports.get_function("_start")?;
-    let _ = func.call(&mut store, &[])?;
+    let mut runtime = underRuntime::CreateInstance(target, bytecode);
 
 /*
     loop{
@@ -54,11 +34,11 @@ fn main() -> Result <()>{
                runtime.extend_bytecode(buf1.to_vec());
             }
         }
-    }
+    }*/
     match runtime.call_function("_start"){
         Ok(_) => println!("Run Successful"),
         Err(_) => println!("Run Faild!"),
-    }*/
+    }
 
     Ok(())
 }
