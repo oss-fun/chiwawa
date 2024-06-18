dcb431683c6e97f6526df87342203cc173d59d43
Yuki <y-nakata@sakura.ad.jp>
2024-06-18 18:28:12 +0900

    Feat: Table Representation

--- src/module.rs
+++ src/module.rs
@@ -5,10 +5,15 @@ pub struct Func {
     type_: TypeIdx,
     locals: Vec<ValueType>,
     body: Expr,
-} 
+}
+
+pub struct Table {
+    type_: TableType,
+}
 
 pub struct Module {
     name: String,
     types: Vec<FuncType>,
     funcs: Vec<Func>,
+    tables: Vec<Table>,
 }
