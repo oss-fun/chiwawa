19fb31ad6352dff1d49de2a53bb15cea275d6f26
Yuki <y-nakata@sakura.ad.jp>
2024-06-19 20:11:17 +0900

    refactor: 💡 Separated Decoding Import Section Process by Func

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use wasmparser::{Parser, Payload::*, TypeRef, ValType};
+use wasmparser::{Parser, Payload::*, TypeRef, ValType,SectionLimited};
 use std::fs::File;
 use std::io::Read;
 use thiserror::Error;
@@ -10,6 +10,72 @@ use crate::types::*;
 enum ParserError {
     #[error("Invalid Version")]
     VersionError,
+}
+
+fn decode_import_section(body: SectionLimited<'_, wasmparser::Import<'_>>, imports: &mut Vec<Import>) -> Result<(), Box<dyn std::error::Error>>{
+    for import in body {
+        let import = import?;
+        let desc = match import.ty {
+            TypeRef::Func(type_index) => {
+                ImportDesc::Func(TypeIdx(type_index))
+            },
+            TypeRef::Table(table_type) => {
+                let max = match table_type.maximum{
+                    Some(m) =>  Some(TryFrom::try_from(m).unwrap()),
+                    None => None
+
+                };
+                let limits = Limits{min: TryFrom::try_from(table_type.initial).unwrap(), max};
+                let reftype = if table_type.element_type.is_func_ref() {
+                    RefType::FuncRef
+                } else {
+                    RefType::ExternalRef
+                };
+
+                ImportDesc::Table(TableType(limits,reftype))
+            },
+            TypeRef::Memory(memory) => {
+                let max = match memory.maximum{
+                    Some(m) =>  Some(TryFrom::try_from(m).unwrap()),
+                    None => None
+
+                };
+                let limits = Limits{min: TryFrom::try_from(memory.initial).unwrap(), max};
+                ImportDesc::Mem(MemType(limits))
+            },
+            TypeRef::Global(global) => {
+                let mut_ = if global.mutable{
+                    Mut::Var
+                } else {
+                    Mut::Const
+                };
+                let value_type = match global.content_type {
+                    ValType::I32 => ValueType::NumType(NumType::I32),
+                    ValType::I64 => ValueType::NumType(NumType::I64),
+                    ValType::F32 => ValueType::NumType(NumType::F32),
+                    ValType::F64 => ValueType::NumType(NumType::F64),
+                    ValType::V128 => ValueType::VecType(VecType::V128),
+                    ValType::Ref(ref_type) => {
+                        if ref_type.is_func_ref() {
+                            ValueType::RefType(RefType::FuncRef)
+                        } else {
+                            ValueType::RefType(RefType::ExternalRef)
+                        }
+                    }
+                };
+                ImportDesc::Global(GlobalType(mut_,value_type))
+            },
+            TypeRef::Tag(_) => todo!()
+        };
+        imports.push(
+            Import{
+                module: Name(import.module.to_string()),
+                name: Name(import.name.to_string()),
+                desc,
+            }
+        );
+    }
+    Ok(())
 }
 
 pub fn parse_bytecode(path: &str) -> Result<(), Box<dyn std::error::Error>> {
@@ -33,72 +99,9 @@ pub fn parse_bytecode(path: &str) -> Result<(), Box<dyn std::error::Error>> {
             }
 
             ImportSection(body) => {
-                for import in body {
-                    let import = import?;
-                    let desc = match import.ty {
-                        TypeRef::Func(type_index) => {
-                            ImportDesc::Func(TypeIdx(type_index))
-                        },
-                        TypeRef::Table(table_type) => {
-                            let max = match table_type.maximum{
-                                Some(m) =>  Some(TryFrom::try_from(m).unwrap()),
-                                None => None
-
-                            };
-                            let limits = Limits{min: TryFrom::try_from(table_type.initial).unwrap(), max};
-                            let reftype = if table_type.element_type.is_func_ref() {
-                                RefType::FuncRef
-                            } else {
-                                RefType::ExternalRef
-                            };
-
-                            ImportDesc::Table(TableType(limits,reftype))
-                        },
-                        TypeRef::Memory(memory) => {
-                            let max = match memory.maximum{
-                                Some(m) =>  Some(TryFrom::try_from(m).unwrap()),
-                                None => None
-
-                            };
-                            let limits = Limits{min: TryFrom::try_from(memory.initial).unwrap(), max};
-                            ImportDesc::Mem(MemType(limits))
-                        },
-                        TypeRef::Global(global) => {
-                            let mut_ = if global.mutable{
-                                Mut::Var
-                            } else {
-                                Mut::Const
-                            };
-                            let value_type = match global.content_type {
-                                ValType::I32 => ValueType::NumType(NumType::I32),
-                                ValType::I64 => ValueType::NumType(NumType::I64),
-                                ValType::F32 => ValueType::NumType(NumType::F32),
-                                ValType::F64 => ValueType::NumType(NumType::F64),
-                                ValType::V128 => ValueType::VecType(VecType::V128),
-                                ValType::Ref(ref_type) => {
-                                    if ref_type.is_func_ref() {
-                                        ValueType::RefType(RefType::FuncRef)
-                                    } else {
-                                        ValueType::RefType(RefType::ExternalRef)
-                                    }
-                                }
-                            };
-                            ImportDesc::Global(GlobalType(mut_,value_type))
-                        },
-                        TypeRef::Tag(_) => todo!()
-                    };
-                    imports.push(
-                        Import{
-                            module: Name(import.module.to_string()),
-                            name: Name(import.name.to_string()),
-                            desc,
-                        }
-                    );
-
-                    println!("{}::{}", import.module, import.name);
-                }
-
+                let _ = decode_import_section(body,&mut imports);
             }
+
             FunctionSection(_) => { /* ... */ }
             TableSection(_) => { /* ... */ }
             MemorySection(_) => { /* ... */ }
