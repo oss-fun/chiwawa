459000adebcaae45ce33188bdf767862b2ff9e15
Yuki <y-nakata@sakura.ad.jp>
2024-06-21 17:13:57 +0900

    feat: 🎸 Define Instructions

--- src/types.rs
+++ src/types.rs
@@ -30,7 +30,12 @@ pub struct TableIdx(pub u32);
 pub struct MemIdx(pub u32); 
 pub struct FuncIdx(pub u32); 
 pub struct GlobalIdx(pub u32); 
+pub struct LocalIdx(pub u32); 
+pub struct LaneIdx(pub u8); 
+pub struct DataIdx(pub u32); 
+pub struct LabelIdx(pub u32); 
 
+pub struct BlockType(pub TypeIdx, pub Option<ValueType>);
 
 pub struct Byte(u8);
 pub struct Name(pub String);
