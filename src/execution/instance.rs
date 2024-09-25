d409597519449390059fa3e93a2b9d1405b806d3
Yuki <y-nakata@sakura.ad.jp>
2024-09-25 18:35:49 +0900

    feat: 🎸 Define Global Instance

--- src/execution/instance.rs
+++ src/execution/instance.rs
@@ -1,13 +1,21 @@
 use crate::structure::types::*;
 use std::{rc::Rc, cell::RefCell};
+use super::value::Val;
 
 struct ModuleInst {
     pub types: Vec<FuncType>,
     pub mem_addrs: Vec<MemAddr>,
+    pub global_addrs: Vec<GlobalAddr>,
 }
 
 struct MemAddr(Rc<RefCell<MemInst>>);
 struct MemInst {
     pub type_: MemType,
     pub data: Vec<u8>,
+}
+
+struct GlobalAddr(Rc<RefCell<GlobalInst>>);
+struct GlobalInst {
+    pub type_: GlobalType,
+    pub value: Val,
 }
