9ad27da821bf75428b7d1e19906ce0bd63fc2213
Yuki <y-nakata@sakura.ad.jp>
2024-09-30 11:28:36 +0900

    feat: 🎸 Create Dummy Module Instance

--- src/execution/module.rs
+++ src/execution/module.rs
@@ -1,4 +1,6 @@
-use crate::structure::types::*;
+use std::rc::Rc;
+use crate::structure::{types::*, module::*};
+use crate::error::RuntimeError;
 use super::value::Val;
 use super::{func::FuncAddr, table::TableAddr, mem::MemAddr, global::GlobalAddr, elem::ElemAddr, data::DataAddr, export::ExportInst};
 
@@ -13,4 +15,20 @@ pub struct ModuleInst {
     pub elem_addrs: Vec<ElemAddr>,
     pub data_addrs: Vec<DataAddr>,
     pub exports: Vec<ExportInst>,
+}
+
+impl ModuleInst {
+    pub fn new(module: Module) -> Result<Rc<ModuleInst>, RuntimeError>{
+        let mut module_inst = ModuleInst {
+            types: module.types.clone(),
+            func_addrs: Vec::new(),
+            table_addrs: Vec::new(),
+            mem_addrs: Vec::new(),
+            global_addrs:Vec::new(),
+            elem_addrs:Vec::new(),
+            data_addrs:Vec::new(),
+            exports:Vec::new(),
+        };    
+        Ok(Rc::new(module_inst))
+    }
 }
