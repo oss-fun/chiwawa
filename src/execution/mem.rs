d6f5fc12fc981f51f01f7f3ac0a4f0a8589e1318
Yuki <y-nakata@sakura.ad.jp>
2024-10-02 15:28:45 +0900

    feat: 🎸 Instantiate Mem

--- src/execution/mem.rs
+++ src/execution/mem.rs
@@ -7,3 +7,14 @@ pub struct MemInst {
     pub type_: MemType,
     pub data: Vec<u8>,
 }
+
+impl MemAddr {
+    pub fn new(type_: &MemType) -> MemAddr{
+        MemAddr(Rc::new(RefCell::new(
+            MemInst{
+                type_: type_.clone(),
+                data: Vec::new(),
+            }
+        )))
+    }
+}
