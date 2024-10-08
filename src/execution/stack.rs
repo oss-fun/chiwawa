e3b5b78067996047f2d5858076cb7d0df60c9b28
Yuki <y-nakata@sakura.ad.jp>
2024-10-08 14:44:29 +0900

    feat: 🎸 Dummy Stacks

---
+++ src/execution/stack.rs
@@ -0,0 +1,15 @@
+use super::{value::*};
+
+pub struct Stacks{
+    values: Vec<Val>,
+    labels: Vec<Label>,
+    activationFrames: Vec<Frame>,
+}
+
+struct Frame{
+    /*Todo Implement Frame Stack*/
+}
+
+struct Label{
+    /*Todo Implement Label Stack*/
+}
