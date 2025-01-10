47719f02d494b288925995357a502f5670c9eced
Yuki <y-nakata@sakura.ad.jp>
2025-01-10 16:52:55 +0900

    feat: 🎸 I64Load Instruction

--- src/execution/stack.rs
+++ src/execution/stack.rs
@@ -1323,6 +1323,12 @@ impl LabelStack{
                             let module_inst = frame.module.upgrade().ok_or_else(||RuntimeError::InstructionFailed).unwrap();
                             self.valueStack.push(Val::Num(Num::I32(module_inst.mem_addrs[0].load::<i32>(&arg, ptr)?)));
                             None
+                        },
+                        Instr::I64Load(arg) =>{
+                            let ptr = self.valueStack.pop().unwrap().to_i32();
+                            let module_inst = frame.module.upgrade().ok_or_else(||RuntimeError::InstructionFailed).unwrap();
+                            self.valueStack.push(Val::Num(Num::I64(module_inst.mem_addrs[0].load::<i64>(&arg, ptr)?)));
+                            None
                         }
                         _ => todo!(),
                     }
