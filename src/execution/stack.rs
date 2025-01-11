02dca71a3d545992bc003789bfeb0af015bcda06
Yuki <y-nakata@sakura.ad.jp>
2025-01-11 19:45:53 +0900

    feat: 🎸 F64Load Instruction

--- src/execution/stack.rs
+++ src/execution/stack.rs
@@ -1335,6 +1335,12 @@ impl LabelStack{
                             let module_inst = frame.module.upgrade().ok_or_else(||RuntimeError::InstructionFailed).unwrap();
                             self.valueStack.push(Val::Num(Num::F32(module_inst.mem_addrs[0].load::<f32>(&arg, ptr)?)));
                             None
+                        },
+                        Instr::F64Load(arg) =>{
+                            let ptr = self.valueStack.pop().unwrap().to_i32();
+                            let module_inst = frame.module.upgrade().ok_or_else(||RuntimeError::InstructionFailed).unwrap();
+                            self.valueStack.push(Val::Num(Num::F64(module_inst.mem_addrs[0].load::<f64>(&arg, ptr)?)));
+                            None
                         }
                         _ => todo!(),
                     }
