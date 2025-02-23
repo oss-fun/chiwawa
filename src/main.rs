1ba10fa3806447f79d07d6e69815ea1953562832
Yuki Nakata <chikuwa.1703@gmail.com>
2025-02-23 22:29:51 +0900

    feat: 🎸 specify func name

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Result};
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use maplit::hashmap;
 use chiwawa::{parser,structure::module::Module,execution::module::*, execution::value::*, execution::func::*};
 use std::collections::HashMap;
@@ -7,22 +7,21 @@ use std::collections::HashMap;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
+struct Cli {
     path: String,
+    #[arg(short, long, default_value = "main")]
+    invoke: String,
 }
 
 
 fn main() -> Result <()>{
-    let args = Args::parse();
+    let args = Cli::parse();
     let mut module = Module::new("test");
-    let _ = parser::parse_bytecode(&mut module, &args.path);
-
+    let _ = parser::parse_bytecode(&mut module, &args.path);        
     let mut imports: ImportObjects = HashMap::new();
-
     let inst = ModuleInst::new(&module, imports).unwrap();
     let params: Vec<Val> = vec![];
-    let ret = inst.get_export_func("mandelbrot")?.call(params);
+    let ret = inst.get_export_func(&args.invoke)?.call(params);
     println!("pi{}", ret?.pop().unwrap().to_f64());
     Ok(())
 }
