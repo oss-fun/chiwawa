03b409f87c3ffdb2130adc89dd704d7939952362
Yuki Nakata <chikuwa.1703@gmail.com>
2026-09-02 13:54:50 +0900

    refactor: 💡 Narrow the visibility of execution modules

--- src/execution.rs
+++ src/execution.rs
@@ -1,20 +1,20 @@
 mod data;
-pub mod dispatch;
+mod dispatch;
 #[cfg(not(feature = "tco"))]
-pub mod dispatch_loop;
+mod dispatch_loop;
 #[cfg(feature = "tco")]
-pub mod dispatch_tco;
+mod dispatch_tco;
 mod elem;
 mod export;
 pub mod func;
 pub(crate) mod global;
-pub mod handlers;
-pub mod ir;
-pub mod mem;
+pub(crate) mod handlers;
+pub(crate) mod ir;
+pub(crate) mod mem;
 pub mod migration;
 pub mod module;
-pub mod operand;
-pub mod regs;
+mod operand;
+pub(crate) mod regs;
 pub mod runtime;
 pub mod state;
 mod table;
